9ea5671b2bffe7016fa6d622e2f6ba3a1f026da7
CedgonzalDevCda <98330296+CedgonzalDevCda@users.noreply.github.com>
2022-05-16 14:08:58 +0200

    feath_favorisList ok

--- boardgameonline-proto/src/Controller/MainController.php
+++ boardgameonline-proto/src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(GameRepository $gameRepository): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'games' => $gameRepository->findAll(),
         ]);
     }
 }

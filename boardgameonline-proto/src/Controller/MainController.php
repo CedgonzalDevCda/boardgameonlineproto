370e111d261e4b9798f42a75b370c0a0a8fb241a
CedgonzalDevCda <98330296+CedgonzalDevCda@users.noreply.github.com>
2022-05-18 12:53:00 +0200

    [fix] filterByCategory no duplicated categories

--- boardgameonline-proto/src/Controller/MainController.php
+++ boardgameonline-proto/src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(GameRepository $gameRepository): Response
+    public function index(GameRepository $gameRepository, CategoryRepository $categoryRepository): Response
     {
         return $this->render('main/index.html.twig', [
             'games' => $gameRepository->findAll(),
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
 }

b8413ed27f544beeff07244965f0753b3a2af22f
CedgonzalDevCda <98330296+CedgonzalDevCda@users.noreply.github.com>
2022-05-25 10:28:36 +0200

    [feath] edit userpassword

--- boardgameonline-proto/src/Controller/UserController.php
+++ boardgameonline-proto/src/Controller/UserController.php
@@ -2,17 +2,76 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserPasswordType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
 class UserController extends AbstractController
 {
+    /**
+     * Cette route permet de visualiser les informations de l'utilisateur
+     *
+     * @return Response
+     */
     #[Route('/user', name: 'app_user')]
     public function index(): Response
     {
         return $this->render('user/index.html.twig', [
             'user' => 'UserController',
+        ]);
+    }
+
+    /**
+     * Cette route permet de modifier le mot de passe de l'utilisateur
+     *
+     * @param User $user
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordHasherInterface $hasher
+     * @return Response
+     */
+    #[Route('/user/change-password/{id}', name: 'app_edit_password', methods: ['GET', 'POST'])]
+    public function editPassword( User $user, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher): Response
+    {
+        $form = $this->createForm(UserPasswordType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($hasher->isPasswordValid($user, $form->getData()['plainPassword'])) {
+                $user->setPassword(
+                    $hasher->hashPassword(
+                        $user,
+                        $form->getData()['newPassword']
+                    )
+                );
+
+                $this->addFlash(
+                    'success',
+                    'Le mot de passe a été modifié.'
+                );
+
+                $manager->persist($user);
+                $manager->flush();
+
+                return $this->redirectToRoute('app_user');
+            } else {
+                $this->addFlash(
+                    'warning',
+                    'Le mot de passe renseigné est incorrect.'
+                );
+            }
+        }
+
+
+        return $this->render('user/edit_Password.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
         ]);
     }
 }
